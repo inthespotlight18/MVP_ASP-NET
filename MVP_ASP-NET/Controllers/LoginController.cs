844505f7e397109cd9d18878eaec50bb0d3a6cf2
inthespotlight17 <64893014+inthespotlight17@users.noreply.github.com>
2022-10-18 19:36:40 -0700

    DZ221018 - registration form created
    Registration form added. It can send data to the webDaniil through absolute path

---
+++ MVP_ASP-NET/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿//using Grpc.Core;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using MVP_ASP_NET.Models;
+
+namespace MVP_ASP_NET.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult ProcessLogin(RegFormsData RegFormsData)
+        {
+            // https://learn.microsoft.com/en-us/dotnet/api/system.uri?view=net-7.0
+
+            //http://gp04.garnet.ca/wcfDaniil/UserAdd?UserName=
+
+            string webDaniilUrl = "http://gp04.garnet.ca/wcfDaniil/UserAdd?UserName=";
+
+            //Only should be used for small data
+            Uri siteUri = new Uri(webDaniilUrl+RegFormsData.Name);     
+
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, siteUri);
+            HttpResponseMessage response = client.Send(request);
+
+            if (RegFormsData.Name == "Daniil" && RegFormsData.Email == "1234")
+            {
+                return View("LoginSucces", RegFormsData);
+            } 
+            else
+            {
+                return View("LoginFailure", RegFormsData);
+            }
+            
+
+        }
+
+    }
+}
---
+++ MVP_ASP-NET/Models/RegFormsData.cs
@@ -0,0 +1,9 @@
+﻿namespace MVP_ASP_NET.Models
+{
+    public class RegFormsData
+    {
+        
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
